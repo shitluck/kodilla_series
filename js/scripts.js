62111b4b4699bb665c4e18d6fc7e8e44156bc98b
Mac Ryndziewicz <fuji.two6@gmail.com>
2017-03-29 16:50:51 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+﻿var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+
+var animalUpperCased = animal.toUpperCase();
+console.log(animalUpperCased);
+
+var textCharsAfter = text.replace('Papugi', animalUpperCased);
+console.log(textCharsAfter);
+
+console.log(textCharsAfter.length/2);
+
+var partOfTextAfterAllChanges = textCharsAfter.slice(0,45);
+console.log(partOfTextAfterAllChanges);
